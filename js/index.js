d654c39e631ba738d65e88da2ce269b5ead89d0c
Ajraa <salajkapavel@gmail.com>
2023-11-05 16:21:01 +0100

    Refactor and added iphone pro page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,4 @@
-let slideIndex = 0;
-
 window.addEventListener("load", function(e) {
-    showSlides(slideIndex);
-    const next = this.document.getElementById("next");
-    console.log(next);
-    next.addEventListener("click", function() {
-        plusSlides(1);
-    });
-    const prev = this.document.getElementById("prev");
-    prev.addEventListener("click", function() {
-        plusSlides(-1);
-    });
-
     let slides = document.getElementsByClassName("slide");
     for (let i = 0; i < slides.length; i++)
     {
@@ -26,26 +13,3 @@ window.addEventListener("load", function(e) {
         });
     }
 });
-
-
-
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
---
+++ js/slider.js
@@ -0,0 +1,37 @@
+let slideIndex = 0;
+
+window.addEventListener("load", function(e) {
+    showSlides(slideIndex);
+    const next = this.document.getElementById("next");
+    console.log(next);
+    next.addEventListener("click", function() {
+        plusSlides(1);
+    });
+    const prev = this.document.getElementById("prev");
+    prev.addEventListener("click", function() {
+        plusSlides(-1);
+    });
+});
+
+
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
